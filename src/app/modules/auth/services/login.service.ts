6698fe6743ae5db4e19db6ccab3b8eae83e3756d
Marco Zamora <mzamora@Marcos-MacBook-Pro.local>
2020-02-23 15:01:31 -0600

    Update login token

--- src/app/modules/auth/services/login.service.ts
+++ src/app/modules/auth/services/login.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { BaseAjaxService } from 'app/modules/base/services/base-ajax.service';
 
 @Injectable()
 export class LoginService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private db: BaseAjaxService) { }
 
-    login(username:string, password:string) {
-        return this.http.post('SYS0001',{
-            email: username,
-            password: password
-        });
+    login(username: string, password: string) {
+      const params = this.db.createParameter('SYS0001', 1, {
+        V7: username,
+        V8: password
+      });
+      this.db.getData(params);
     }
 }
